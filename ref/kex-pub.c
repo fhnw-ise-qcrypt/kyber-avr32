82211885163fd221d96abef1b9548f1a9e0709fe
Simon Burkhardt <simon@simonmartin.ch>
2021-09-07 07:51:55 +0200

    added file-IO method to transfer kex messages

--- ref/kex-pub.c
+++ ref/kex-pub.c
@@ -70,87 +70,93 @@ int main( int argc, char *argv[] )
     // save temporary variables
     writeHexFile("./TK.key", tk, KEX_SSBYTES);
     writeHexFile("./ESKA.key", eska, CRYPTO_SECRETKEYBYTES);
+    writeHexFile("./ake_senda.txt", ake_senda, KEX_AKE_SENDABYTES);
     // send to Bob
-    printHexString("Send to other Party:", ake_senda, KEX_AKE_SENDABYTES);
+    printf("Send \"ake_senda.txt\" to other party.\n");
+    // printHexString("Send to other Party:", ake_senda, KEX_AKE_SENDABYTES);
   }
 
   // continue as Bob
   else if( argv[1][0] == '-' && argv[1][1] == 'B' ){
+    uint8_t ka[KEX_SSBYTES];
+    uint8_t ska[CRYPTO_SECRETKEYBYTES];
+    uint8_t ake_senda[KEX_AKE_SENDABYTES];
+    uint8_t ake_sendb[KEX_AKE_SENDBBYTES];
+    uint8_t pkb[CRYPTO_PUBLICKEYBYTES];
+
+    for(i=0;i<KEX_SSBYTES;i++) ka[i] = 0;
+    for(i=0;i<CRYPTO_SECRETKEYBYTES;i++) ska[i] = 0;
+    for(i=0;i<CRYPTO_PUBLICKEYBYTES;i++) pkb[i] = 0;
+    for(i=0;i<KEX_AKE_SENDABYTES;i++) ake_senda[i] = 0;
+    for(i=0;i<KEX_AKE_SENDBBYTES;i++) ake_sendb[i] = 0;
+
     if( argc > 2){
-      uint8_t ka[KEX_SSBYTES];
-      uint8_t ska[CRYPTO_SECRETKEYBYTES];
-      uint8_t ake_senda[KEX_AKE_SENDABYTES];
-      uint8_t ake_sendb[KEX_AKE_SENDBBYTES];
-      uint8_t pkb[CRYPTO_PUBLICKEYBYTES];
-
-      for(i=0;i<KEX_SSBYTES;i++) ka[i] = 0;
-      for(i=0;i<CRYPTO_SECRETKEYBYTES;i++) ska[i] = 0;
-      for(i=0;i<CRYPTO_PUBLICKEYBYTES;i++) pkb[i] = 0;
-      for(i=0;i<KEX_AKE_SENDABYTES;i++) ake_senda[i] = 0;
-      for(i=0;i<KEX_AKE_SENDBBYTES;i++) ake_sendb[i] = 0;
-
       // read command line argument from other party
       stdinHexString((char*)argv[2], ake_senda, KEX_AKE_SENDABYTES);
-
-      // retreive stored own private key
-      readHexFile("./SKA.key", ska, CRYPTO_SECRETKEYBYTES);
-      readHexFile("./PKB.key", pkb, CRYPTO_PUBLICKEYBYTES);
-
-      // from the perspective of Bob it should be:
-      // skb = ska / pka = pkb 
-      // but it is not! WHY ???
-      kex_ake_sharedB(ake_sendb, ka, ake_senda, ska, pkb); // Run by Bob
-      // kex_ake_sharedB(ake_sendb, kb, ake_senda, skb, pkb);
-
-      printHexString("Send to other Party: ", ake_sendb, KEX_AKE_SENDBBYTES);
-      printHexString("Common Shared Secret: ", ka, KEX_SSBYTES);
-
-      // save common key
-      writeHexFile("./COMMON.key", ka, KEX_SSBYTES);
-
+    } else if( access("./ake_senda.txt", F_OK ) == 0 ) {
+      readHexFile("./ake_senda.txt", ake_senda, KEX_AKE_SENDABYTES); 
     } else {
-      printf("error -B: no message provided.\n");
+      printf("error -B: no message (stdin or file) provided.\n");
       return -1;
     }
+    // retreive stored own private key
+    readHexFile("./SKA.key", ska, CRYPTO_SECRETKEYBYTES);
+    readHexFile("./PKB.key", pkb, CRYPTO_PUBLICKEYBYTES);
+
+    // from the perspective of Bob it should be:
+    // skb = ska / pka = pkb 
+    // but it is not! WHY ???
+    kex_ake_sharedB(ake_sendb, ka, ake_senda, ska, pkb); // Run by Bob
+    // kex_ake_sharedB(ake_sendb, kb, ake_senda, skb, pkb);
+    writeHexFile("./ake_sendb.txt", ake_sendb, KEX_AKE_SENDBBYTES);
+
+    //printHexString("Send to other party: ", ake_sendb, KEX_AKE_SENDBBYTES);
+    printf("Send \"ake_sendb.txt\" to other party.\n");
+    printHexString("Common secret: ", ka, KEX_SSBYTES);
+
+    // save common key
+    writeHexFile("./COMMON.key", ka, KEX_SSBYTES);
   }
 
   // finish as Alice
   else if( argv[1][0] == '-' && argv[1][1] == 'C' ){
-    if( argc > 2){
-      if( access("./ESKA.key", F_OK ) == 0 && access("./TK.key", F_OK ) == 0 ) {
-        uint8_t tk[KEX_SSBYTES];
-        uint8_t ka[KEX_SSBYTES];
-        uint8_t eska[CRYPTO_SECRETKEYBYTES];
-        uint8_t ska[CRYPTO_SECRETKEYBYTES];
-        uint8_t ake_sendb[KEX_AKE_SENDBBYTES];
+    uint8_t tk[KEX_SSBYTES];
+    uint8_t ka[KEX_SSBYTES];
+    uint8_t eska[CRYPTO_SECRETKEYBYTES];
+    uint8_t ska[CRYPTO_SECRETKEYBYTES];
+    uint8_t ake_sendb[KEX_AKE_SENDBBYTES];
 
-        for(i=0;i<KEX_SSBYTES;i++) ka[i] = 0;
-        for(i=0;i<KEX_SSBYTES;i++) tk[i] = 0;
-        for(i=0;i<CRYPTO_SECRETKEYBYTES;i++) eska[i] = 0;
-        for(i=0;i<CRYPTO_SECRETKEYBYTES;i++) ska[i] = 0;
-        for(i=0;i<KEX_AKE_SENDBBYTES;i++) ake_sendb[i] = 0;
+    for(i=0;i<KEX_SSBYTES;i++) ka[i] = 0;
+    for(i=0;i<KEX_SSBYTES;i++) tk[i] = 0;
+    for(i=0;i<CRYPTO_SECRETKEYBYTES;i++) eska[i] = 0;
+    for(i=0;i<CRYPTO_SECRETKEYBYTES;i++) ska[i] = 0;
+    for(i=0;i<KEX_AKE_SENDBBYTES;i++) ake_sendb[i] = 0;
 
-        // retreive stored TK variable
-        readHexFile("./TK.key", tk, KEX_SSBYTES);
-        readHexFile("./SKA.key", ska, CRYPTO_SECRETKEYBYTES);
-        readHexFile("./ESKA.key", eska, CRYPTO_SECRETKEYBYTES);
-
-        // read command line argument from other party
-        stdinHexString((char*)argv[2], ake_sendb, KEX_AKE_SENDBBYTES);
-
-        kex_ake_sharedA(ka, ake_sendb, tk, eska, ska); // Run by Alice
-        // kex_ake_sharedA(ka, ake_sendb, tk, eska, ska);
-
-        printHexString("Common Shared Secret: ", ka, KEX_SSBYTES);
-
-        // save common key
-        writeHexFile("./COMMON.key", ka, KEX_SSBYTES);
-      } else {
-        printf("error: the other party's public key is not known.\n");
-      }
+    if( argc > 2 ){
+      // read command line argument from other party
+      stdinHexString((char*)argv[2], ake_sendb, KEX_AKE_SENDBBYTES);
+    } else if( access("./ake_sendb.txt", F_OK ) == 0 ) {
+      readHexFile("./ake_sendb.txt", ake_sendb, KEX_AKE_SENDBBYTES);
     } else {
       printf("error -s: no key provided.\n");
       return -1;
+    }
+    if( access("./ESKA.key", F_OK ) == 0 && access("./TK.key", F_OK ) == 0 ) {
+
+      // retreive stored TK variable
+      readHexFile("./TK.key", tk, KEX_SSBYTES);
+      readHexFile("./SKA.key", ska, CRYPTO_SECRETKEYBYTES);
+      readHexFile("./ESKA.key", eska, CRYPTO_SECRETKEYBYTES);
+
+      kex_ake_sharedA(ka, ake_sendb, tk, eska, ska); // Run by Alice
+      // kex_ake_sharedA(ka, ake_sendb, tk, eska, ska);
+
+      printHexString("Common secret: ", ka, KEX_SSBYTES);
+
+      // save common key
+      writeHexFile("./COMMON.key", ka, KEX_SSBYTES);
+    } else {
+      printf("error: the other party's public key is not known.\n");
     }
   }
 
